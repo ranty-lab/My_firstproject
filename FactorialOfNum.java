1dfc063cdccaf5bbb53fed4d1332fa4233586853
ranty-lab <rantybarman2@gmail.com>
2026-02-22 03:43:27 +0600

    Add files via upload

---
+++ FactorialOfNum.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class FactorialOfNum {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int num, fact = 1;
+
+        System.out.print("Enter any positive integer: ");
+        num = input.nextInt();
+
+        for(int i = num; i >= 1; i--){
+            fact = fact * i;
+        }
+
+        System.out.println("Factorial of " + num + " = " + fact);
+    }
+}
---
+++ PrimeNumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+               System.out.print("Enter any positive integer: ");
+       int num = input.nextInt();
+       int count =0;
+        for(int i=2; i < num; i++){
+            if (num%i==0){
+                count++;
+                break;
+            }
+           
+        }
+        if (count ==0){
+             System.out.println("Prime number");
+
+        }else{ System.out.println("Not prime");
+
+        }
+           }
+}
