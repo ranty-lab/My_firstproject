97664f89db049c98bdddc6f833355d0666f9c6c7
ranty-lab <rantybarman2@gmail.com>
2026-05-08 02:33:00 +0600

    Add files via upload

---
+++ GoldbachConjecture.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class GoldbachConjecture {
+    // Function to check prime number
+    static boolean isPrime(int n) {
+        if (n < 2) {
+            return false;
+        }
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter an even number: ");
+        int num = sc.nextInt();
+        for (int i = 2; i <= num / 2; i++) {
+            if (isPrime(i) && isPrime(num - i)) {
+                System.out.println(num + " = " + i + " + " + (num - i));
+            }
+        }
+        sc.close();
+    }
+}
