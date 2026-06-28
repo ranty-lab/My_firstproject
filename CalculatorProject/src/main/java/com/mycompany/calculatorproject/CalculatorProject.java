eb4d22368eb0b594d533b94375281aad6d061288
ranty-lab <rantybarman2@gmail.com>
2026-06-28 18:19:35 +0600

    Add files via upload

---
+++ CalculatorProject/src/main/java/com/mycompany/calculatorproject/CalculatorProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculatorproject;
+
+/**
+ *
+ * @author BUSHRA-IT
+ */
+public class CalculatorProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
